3feeee9a94804689d1be24634162e89983ab194c
lbotta <lbotta@iu.edu>
2023-04-20 00:32:11 -0400

    updated navbar with logout button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "../Styles/Styles.css";
+import { useEffect, useState } from "react";
 function navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    if (sessionStorage.loggedInToken == "") {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  });
   return (
     <nav class="navbar fixed-top navbar-light navbar-container nav-bar-container">
       <div class="sub-nav-container nav-padding-left">
@@ -14,14 +23,23 @@ function navbar() {
           Dashboards
         </a>
       </div>
-      <div class="sub-nav-container">
-        <a class="nav-link nav-item login-btn" href="/login">
-          Login
-        </a>
-        <a class="nav-link nav-item sign-up-btn" href="/signup">
-          Register
-        </a>
-      </div>
+      {!isLoggedIn && (
+        <div class="sub-nav-container">
+          <a class="nav-link nav-item login-btn" href="/login">
+            Login
+          </a>
+          <a class="nav-link nav-item sign-up-btn" href="/signup">
+            Register
+          </a>
+        </div>
+      )}
+      {isLoggedIn && (
+        <div class="sub-nav-container">
+          <a class="nav-link nav-item login-btn" href="/login">
+            Logout
+          </a>
+        </div>
+      )}
     </nav>
   );
 }
